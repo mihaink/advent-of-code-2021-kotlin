5b7a38625861480f3904a297b49fbfbbb0ee0692
Mihai <mihai303@gmail.com>
2021-12-13 10:54:19 +0200

    Add full solution for Day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -3,19 +3,16 @@ import java.lang.Integer.max
 import kotlin.math.abs
 
 data class Line(val x1: Int, val y1: Int, val x2: Int, val y2: Int) {
-    fun getPoints(): List<Pair<Int, Int>> {
+    private fun getPoints(): List<Pair<Int, Int>> {
         return if (x1 == x2 && y1 != y2) {
             List(abs(y1-y2) + 1) { x1 } zip min(y1,y2)..max(y1, y2)
         } else if (x1 != x2 && y1 == y2) {
             min(x1, x2)..max(x1, x2) zip List(abs(x1-x2) + 1) { y1 }
-        } else if (x1 == y1 && x2 == y2) {
-            min(x1,x2) .. max(x1, x2) zip  min(x1,x2) .. max(x1, x2)
         } else {
-
-            mutableListOf()
+            val xRange = if (x1 < x2) x1..x2 else x1 downTo x2
+            val yRange = if (y1 < y2) y1.. y2 else y1 downTo y2
+            (xRange) zip (yRange)
         }
-
-
     }
 
     fun intersect(secondLine: Line): Set<Pair<Int, Int>> {
@@ -55,40 +52,27 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val allLines = input.map { parseLine(it) }
-        val filteredLines = allLines
+        val lines = input.map { parseLine(it) }
 
-        println( Line(6, 4, 2, 0).getPoints() )
-
-
-//        val filteredLines = allLines.filter {
-////            (it.x1 == it.x2 && it.y1 != it.y2) || (it.y1 == it.y2 && it.x1 != it.x2) || (it.x1 == it.y1 && it.x2 == it.y2) || (it.x1 == it.y2 && it.x2 == it.y1)
-//        }
-
-//        println (filteredLines)
 
         val commonPoints = mutableSetOf<Pair<Int,Int>>()
 
-        for (index in 0.. filteredLines.size - 2) {
+        for (index in 0.. lines.size - 2) {
 
-            for (innerIndex in index+1 until filteredLines.size) {
-                commonPoints.addAll(filteredLines[index].intersect(filteredLines[innerIndex]))
+            for (innerIndex in index+1 until lines.size) {
+                commonPoints.addAll(lines[index].intersect(lines[innerIndex]))
             }
         }
-
-//        commonPoints.forEach { point -> println(point) }
 
         return commonPoints.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    val testInputPart1 = readInput("Day05")
     check(part1(testInput) == 5)
-    check(part1(testInputPart1) == 4655)
     check(part2(testInput) == 12)
 
-//    val input = readInput("Day05")
-//    println(part1(input))
-//    println(part2(input))
+    val input = readInput("Day05")
+    println(part1(input))
+    println(part2(input))
 }
